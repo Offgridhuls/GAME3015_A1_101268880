50a7bde713011379c2bc7575e5be9539f58fbe95
Offgridhuls <55862297+Offgridhuls@users.noreply.github.com>
2022-03-30 22:11:39 -0400

    Command queue

---
+++ Command.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <functional>
+#include "Common/d3dApp.h"
+#include <cassert>
+
+class SceneNode;
+
+struct Command
+{
+	Command();
+
+	std::function<void(SceneNode&, GameTimer)> action;
+
+	unsigned int category;
+};
+
+template <typename GameObject, typename Function>
+std::function<void(SceneNode&, GameTimer)> derivedAction(Function fn)
+{
+	return [=](SceneNode& node, GameTimer dt)
+	{
+		assert(dynamic_cast<GameObject*>(&node) != nullptr);
+
+		fn(static_cast<GameObject&)(node), dt);
+	}
+
+}
